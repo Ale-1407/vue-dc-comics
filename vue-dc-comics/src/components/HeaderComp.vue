66853b60d822a054fd24ce37fb0e310bb3be05fd
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-23 17:48:44 +0100

    inserite immagini e completato header

---
+++ vue-dc-comics/src/components/HeaderComp.vue
@@ -0,0 +1,63 @@
+<template>
+    <header>
+        <img src="../assets/img/dc-logo.png" alt="logo" class="logo">
+        <nav>
+            <ul>
+                <li>CHARACTERS</li>
+                <li class="active">COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+}
+</script>
+
+<style lang="scss">
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+header{
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-around;
+}
+
+nav{
+    margin: auto 0;
+}
+
+ul{
+  display: flex;
+  align-items: center;
+
+  li{
+    padding: 10px;
+    list-style-type: none;
+    font-weight: bold;
+    font-size: 13px;
+  }
+}
+
+.active{
+    color: dodgerblue;   
+}
+
+.logo{
+    height: 90px;
+}
+</style>
